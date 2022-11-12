5b1412f857c11ba0eb0839e3e4f7322dbb37df23
Anuvind830 <111210269+Anuvind830@users.noreply.github.com>
2022-11-12 16:31:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from Pyrogram import Client, filters
 
+API_ID = ""
+API_HASE = ""
+BOT_TOKEN = ""
+
+
+ANUVIND = Client(
+    name="TelegramBot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+    
